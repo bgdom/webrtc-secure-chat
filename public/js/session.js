fbdcf626de26d7af5b40246b481e21be5a57bb0f
Mus <mastour.mustapha@gmail.com>
2020-01-15 16:49:57 +0100

    same page session

---
+++ public/js/session.js
@@ -0,0 +1,11 @@
+(function ($) {
+    "use strict"; // Start of use strict
+
+    $('#get-session-btn').click(function () {
+        console.log("test")
+        $('.intro').css('visibility', 'hidden')
+        $('.session').css('visibility', 'visible')
+    })
+
+
+})(jQuery); // End of use strict
